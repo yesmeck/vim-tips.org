970d393d8f7d5a8427f067091730cc85ee8c306f
Meck <yesmeck@gmail.com>
2012-06-02 16:02:34 +0800

    Add validates for tip

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,3 +1,7 @@
 class Tip < ActiveRecord::Base
   attr_accessible :content, :name, :twitter_id
+
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :name, :length => { :maximum => 20 }
+  validates :twitter_id, :length => { :maximum => 20 }
 end
