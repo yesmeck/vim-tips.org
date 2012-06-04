9e112f1c16dbdceb1e0197acb739709486c4bb53
Meck <yesmeck@gmail.com>
2012-06-04 22:49:19 +0800

    Set approved accessible

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,5 +1,5 @@
 class Tip < ActiveRecord::Base
-  attr_accessible :content, :name, :twitter_id, :url
+  attr_accessible :content, :name, :twitter_id, :url, :approved
 
   validates :content, :presence => true, :length => { :maximum => 140 }
   validates :name, :length => { :maximum => 20 }
