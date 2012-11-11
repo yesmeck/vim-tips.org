975db0e5f73ee17ac25e3eb50ff5eeaea0b5fd70
Meck <yesmeck@gmail.com>
2012-11-11 21:51:39 +0800

    Remove redundant slash

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,7 +27,7 @@ working_directory "#{APP_PATH}/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "#{APP_PATH}//tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{APP_PATH}/tmp/sockets/unicorn.sock", :backlog => 64
 #listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
